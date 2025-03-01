5c3816c0a8cfcc7f8465b676083babe7e043ff89
Gordon Inggs <gordon.inggs@capetown.gov.za>
2025-03-01 13:43:08 +0200

    Reducing startup timeout and increasing concurrency
    The dag runs every 10 minutes anyway

--- src/dags/service-alerts-pipeline.py
+++ src/dags/service-alerts-pipeline.py
@@ -12,23 +12,29 @@ with kubernetes_dag.airflowK8sDAG("service-alerts-pipeline",
                                   start_date=DAG_STARTDATE, schedule_interval=timedelta(minutes=10),
                                   code_location='https://lake.capetown.gov.za/service-alerts-connector.deploy/service-alerts-connector.zip',
                                   install_gs_utils=True,
-                                  concurrency=2) as dag:
+                                  concurrency=2,
+                                  max_active_runs=2) as dag:
     # Operators
     fetch_data_operator = dag.get_dag_operator("fetch-service-alerts",
                                                "python3 cct_connector/ServiceAlertConnector.py",
-                                               resources=kubernetes_dag.LIGHT_RESOURCES)
+                                               resources=kubernetes_dag.LIGHT_RESOURCES,
+                                               startup_timeout_seconds=600)
     fix_data_operator = dag.get_dag_operator("fix-service-alerts",
                                              "python3 cct_connector/ServiceAlertFixer.py",
-                                             resources=kubernetes_dag.LIGHT_RESOURCES)
+                                             resources=kubernetes_dag.LIGHT_RESOURCES,
+                                               startup_timeout_seconds=600)
     augment_data_operator = dag.get_dag_operator("augment-service-alerts",
                                                  "python3 cct_connector/ServiceAlertAugmenter.py",
-                                                 resources=kubernetes_dag.LIGHT_RESOURCES)
+                                                 resources=kubernetes_dag.LIGHT_RESOURCES,
+                                               startup_timeout_seconds=600)
     broadcast_data_operator = dag.get_dag_operator("broadcast-service-alerts",
                                                    "python3 cct_connector/ServiceAlertBroadcaster.py",
-                                                   resources=kubernetes_dag.LIGHT_RESOURCES)
+                                                   resources=kubernetes_dag.LIGHT_RESOURCES,
+                                               startup_timeout_seconds=600)
     email_data_operator = dag.get_dag_operator("email-service-alerts",
                                                "python3 cct_connector/ServiceAlertEmailer.py",
-                                               resources=kubernetes_dag.LIGHT_RESOURCES)
+                                               resources=kubernetes_dag.LIGHT_RESOURCES,
+                                               startup_timeout_seconds=600)
 
     # Dependencies
     fetch_data_operator >> fix_data_operator >> augment_data_operator >> (broadcast_data_operator, email_data_operator)
