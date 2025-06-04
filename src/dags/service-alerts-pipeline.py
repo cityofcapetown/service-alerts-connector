f0f1eaf81056cd7e40f4b3dd021b79f653b8e322
Gordon Inggs <gordon.inggs@capetown.gov.za>
2025-06-04 10:07:02 +0200

    Updating dag to use multiple broadcast operators

--- src/dags/service-alerts-pipeline.py
+++ src/dags/service-alerts-pipeline.py
@@ -12,7 +12,7 @@ with kubernetes_dag.airflowK8sDAG("service-alerts-pipeline",
                                   start_date=DAG_STARTDATE, schedule_interval=timedelta(minutes=10),
                                   code_location='https://lake.capetown.gov.za/service-alerts-connector.deploy/service-alerts-connector.zip',
                                   install_gs_utils=True,
-                                  concurrency=2,
+                                  concurrency=3,
                                   max_active_runs=2) as dag:
     # Operators
     fetch_data_operator = dag.get_dag_operator("fetch-service-alerts",
@@ -30,11 +30,21 @@ with kubernetes_dag.airflowK8sDAG("service-alerts-pipeline",
                                                  resources=kubernetes_dag.LIGHT_RESOURCES,
                                                  startup_timeout_seconds=600,
                                                  task_concurrency=1, )
-    broadcast_data_operator = dag.get_dag_operator("broadcast-service-alerts",
-                                                   "python3 cct_connector/ServiceAlertBroadcaster.py",
-                                                   resources=kubernetes_dag.LIGHT_RESOURCES,
-                                                   startup_timeout_seconds=600,
-                                                   task_concurrency=1, )
+    all_broadcast_data_operator = dag.get_dag_operator("broadcast-service-alerts",
+                                                       "python3 cct_connector/ServiceAlertBroadcaster.py",
+                                                       resources=kubernetes_dag.LIGHT_RESOURCES,
+                                                       startup_timeout_seconds=600,
+                                                       task_concurrency=1, )
+    week_broadcast_data_operator = dag.get_dag_operator("broadcast-service-alerts",
+                                                        "python3 cct_connector/ServiceAlert7DaysBroadcaster.py",
+                                                        resources=kubernetes_dag.LIGHT_RESOURCES,
+                                                        startup_timeout_seconds=600,
+                                                        task_concurrency=1, )
+    current_broadcast_data_operator = dag.get_dag_operator("broadcast-service-alerts",
+                                                           "python3 cct_connector/ServiceAlertCurrentBroadcaster.py",
+                                                           resources=kubernetes_dag.LIGHT_RESOURCES,
+                                                           startup_timeout_seconds=600,
+                                                           task_concurrency=1, )
     email_data_operator = dag.get_dag_operator("email-service-alerts",
                                                "python3 cct_connector/ServiceAlertEmailer.py",
                                                resources=kubernetes_dag.LIGHT_RESOURCES,
@@ -47,5 +57,8 @@ with kubernetes_dag.airflowK8sDAG("service-alerts-pipeline",
                                             task_concurrency=1, )
 
     # Dependencies
-    fetch_data_operator >> fix_data_operator >> augment_data_operator >> (broadcast_data_operator, email_data_operator)
-    broadcast_data_operator >> wa_data_operator
+    fetch_data_operator >> fix_data_operator >> augment_data_operator >> (all_broadcast_data_operator,
+                                                                          week_broadcast_data_operator,
+                                                                          current_broadcast_data_operator,
+                                                                          email_data_operator)
+    current_broadcast_data_operator >> wa_data_operator
