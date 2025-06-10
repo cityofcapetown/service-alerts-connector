b7e5d4fb6ea21f543ddf5cc44e252f7dc861c8b2
Gordon Inggs <gordon.inggs@capetown.gov.za>
2025-06-10 09:16:25 +0200

    Updating examples in README and example script
    Using V1.3, also more recent openapi-generator output

--- bin/api_example_script.py
+++ bin/api_example_script.py
@@ -4,7 +4,7 @@ import cct_service_alerts
 
 with cct_service_alerts.ApiClient() as api_client:
     # Create an instance of the API class
-    api_instance = cct_service_alerts.V1Api(api_client)
-    unplanned_alerts = api_instance.v1_coct_service_alerts_current_unplanned_json_get()
+    api_instance = cct_service_alerts.V13Api(api_client)
+    unplanned_alerts = api_instance.v13_service_alerts_time_frame_planned_get("current", "unplanned")
 
-pprint.pprint(unplanned_alerts)
+pprint.pprint(unplanned_alerts[:2])
